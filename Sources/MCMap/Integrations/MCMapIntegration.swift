fb8875753c9d27ffd1ec617fbeee8bffde457e49
Marquis Kurt <software@marquiskurt.net>
2025-07-20 15:00:28 -0400

    Document MCMapIntegration

--- Sources/MCMap/Integrations/MCMapIntegration.swift
+++ Sources/MCMap/Integrations/MCMapIntegration.swift
@@ -7,8 +7,19 @@
 
 import Foundation
 
+/// A protocol that defines an integration supported by the `.mcmap` format.
+///
+/// Integrations are used to connect to external services to provide additional information. Depending on the client
+/// implementation, this may include making network requests.
 public protocol MCMapIntegration: Codable, Sendable, Equatable, Hashable {
+    /// The key for the integration.
+    ///
+    /// This is used to register the file in the ``CartographyMapFile/Keys/integrations`` subdirectory.
     static var integrationKey: String { get }
 
+    /// Whether the integration is enabled.
+    ///
+    /// Unless this integration is required out of the box, it is generally recommended to keep this as false by
+    /// default.
     var enabled: Bool { get set }
 }
